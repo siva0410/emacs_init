8b7b75291c9aaed0f2070f7c2c0c95af763655b7
siva0410 <42541197+siva0410@users.noreply.github.com>
2025-05-19 10:22:00 +0100

    Set up corfu and LSP servers

--- init.el
+++ init.el
@@ -57,6 +57,7 @@
   :bind (
          ("C-s" . consult-line)
          ("C-x b" . consult-buffer)
+	 ("M-g" . consult-goto-line)
          )
   )
 
@@ -75,14 +76,58 @@
   ;; package.
   (marginalia-mode))
 
-;; ;; ivy
-;; (unless (package-installed-p 'counsel)
-;;   (package-install 'counsel))
-;; (ivy-mode t)
-;; (counsel-mode t)
-;; (define-key global-map (kbd "C-s") 'swiper)
-;; (define-key global-map (kbd "C-S-s") 'counsel-imenu)
-;; (setq ivy-height 15)
+(use-package corfu
+  :bind (
+         :map corfu-map
+         ;; ("TAB" . corfu-insert)
+         ;; ("<tab>" . corfu-insert)
+         ("RET" . nil)
+         ;; ("<return>" . nil)
+	 )
+  
+  ;; Optional customizations
+  :custom
+  (corfu-auto t)
+  (corfu-auto-delay 0)
+  (corfu-auto-prefix 2)
+  (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+  (corfu-preselect 'first)      ;; Preselect the prompt
+  (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+  (tab-always-indent 'complete)
+  
+  ;; Enable Corfu only for certain modes. See also `global-corfu-modes'.
+  ;; :hook ((prog-mode . corfu-mode)
+  ;;        (shell-mode . corfu-mode)
+  ;;        (eshell-mode . corfu-mode))
+  
+  :init
+  ;; Recommended: Enable Corfu globally.  Recommended since many modes provide
+  ;; Capfs and Dabbrev can be used globally (M-/).  See also the customization
+  ;; variable `global-corfu-modes' to exclude certain modes.
+  (global-corfu-mode)
+  ;; Enable auto completion and configure quitting
+  ;; Enable optional extension modes:
+  ;; (corfu-history-mode)
+  ;; (corfu-popupinfo-mode)
+  )
+
+;; (use-package cape
+;;   :init
+;;   (add-to-list 'completion-at-point-functions #'cape-capf-buster))
+(use-package cape
+  :init
+  (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+  (add-to-list 'completion-at-point-functions #'cape-file)
+  )
+
+;; magit
+(use-package magit
+  :bind ("C-x g" . magit-status)
+  )
 
 ;; vterm
 (use-package vterm
@@ -93,35 +138,16 @@
   (setq vterm-keymap-exceptions '("<f1>" "M-[" "M-]" "C-c" "C-x" "C-u" "C-g" "C-l" "C-t" "M-x" "M-o" "C-v" "M-v" "C-y" "M-y"))
   (setq vterm-max-scrollback 10000)
   (setq vterm-buffer-name-string "vterm: %s")
+  (define-key vterm-mode-map (kbd "C-t") 'other-window-or-split)
   )
 
-;; magit
-(use-package magit
-  :bind ("C-x g" . magit-status)
-  )
-
-;; ;;    +---------------------------------------------+
-;; ;;    |                  Key Config                 |
-;; ;;    +---------------------------------------------+
-(defun other-window-or-split ()
-  (interactive)
-  (when (one-window-p)
-    (split-window-horizontally))
-  (other-window 1))
-
-;; (define-key global-map (kbd "C-h") 'delete-backward-char)
-;; (define-key global-map (kbd "C-t") 'other-window-or-split)
-;; (define-key global-map (kbd "C-;") 'comment-dwim)
-;; (define-key global-map (kbd "C-x C-d") 'dired-jump)
-
-;; (define-key dired-mode-map (kbd "C-t") 'other-window-or-split)
+;;    +---------------------------------------------+
+;;    |                  Key Config                 |
+;;    +---------------------------------------------+
+;; (define-key vterm-mode-map (kbd "C-t") 'other-window-or-split)
 ;; ;;(define-key dired-mode-map (kbd "f8") 'toggle-frame-fullscreen)
 
 ;;; Theme
-;; (use-package color-theme-sanityinc-tomorrow
-;;   :init
-;;   (load-theme 'color-theme-sanityinc-tomorrow-night t))
-
 (use-package doom-themes
   :custom
   (doom-themes-enable-italic t)
@@ -148,6 +174,9 @@
   :config
   (line-number-mode t)
   (column-number-mode t)
+  ;; (display-battery-mode t)
+  (setq display-time-24hr-format t)
+  (display-time-mode t)
   )
 
 (use-package rainbow-delimiters
@@ -156,6 +185,26 @@
 ;;    +---------------------------------------------+
 ;;    |                System Config                |
 ;;    +---------------------------------------------+
+;;; LSP
+(use-package eglot
+  :ensure nil
+  :hook ((python-mode . eglot-ensure)
+         (c-mode . eglot-ensure)
+         (c++-mode . eglot-ensure)
+         ;; (go-mode . eglot-ensure)
+         ;; (rust-mode . eglot-ensure)
+         )
+  :config
+  (add-to-list 'eglot-server-programs
+               '(python-mode . ("pyright-langserver" "--stdio")))
+  (add-to-list 'eglot-server-programs
+               '((c-mode c++-mode) . ("clangd")))
+  )
+
+;; (with-eval-after-load 'eglot
+;;   (add-to-list 'eglot-server-programs
+;;                '(python-mode . ("pyright-langserver" "--stdio"))))
+
 (use-package emacs
   :init
   (add-to-list 'default-frame-alist '(fullscreen . maximized)) ; or fullboth
@@ -168,22 +217,7 @@
   (setq frame-title-format "%f")
   (global-display-line-numbers-mode t)
   (tool-bar-mode 0)
-  (menu-bar-mode 0)
-  
-  ;;; Mode Line
-  ;; (column-number-mode t)
-  ;; (display-battery-mode t)
-  ;; (setq display-time-24hr-format t)
-  ;; (display-time-mode t)
-  
-  ;;; Indent
-  ;; (setq-default tab-width 4)
-  ;; (setq-default indent-tabs-mode nil)
-
-  ;;; Parentheses
-  ;; (setq show-paren-delay 0)
-  ;; (setq show-paren-style 'expression)
-  ;; (set-face-background 'show-paren-match "pink")
+  (menu-bar-mode 0)  
   
   (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
   (when (file-exists-p custom-file)
@@ -192,7 +226,7 @@
   (setq make-backup-files nil)
   (setq auto-save-default nil)
   (setq ring-bell-function 'ignore)
-  (defalias 'yes-or-no-p 'y-or-n-p)
+  ;; (defalias 'yes-or-no-p 'y-or-n-p)
   
   ;; scratch buffer
   (setq initial-scratch-message nil)
@@ -200,44 +234,49 @@
   :bind (("C-h" . delete-backward-char)
          ("C-t" . other-window-or-split)
          ("C-;" . comment-dwim)
-         ("C-x C-d" . dired-jump))
+         ("C-x C-d" . dired-jump)
+	 ("C-a" . my-smart-beginning-of-line)
+	 ("C-e" . my-smart-end-of-line))
   :config
   (load-theme 'doom-dracula t)
   )
 
-;; (defvar my-smart-jump-state nil
-;;   "Stores state for smart beginning/end of line navigation.")
+(defvar my-smart-jump-state nil
+  "Stores state for smart beginning/end of line navigation.")
 
-;; (defun my-smart-beginning-of-line ()
-;;   "Toggle between beginning of line, beginning of buffer, and original point."
-;;   (interactive)
-;;   (cond
-;;    ;; 1回目: 行頭へ移動し、元の位置を保存
-;;    ((not (eq last-command 'my-smart-beginning-of-line))
-;;     (setq my-smart-jump-state (list :origin (point) :step 1))
-;;     (move-beginning-of-line 1))
-;;    ;; 2回目: バッファ先頭へ
-;;    ((= (plist-get my-smart-jump-state :step) 1)
-;;     (plist-put my-smart-jump-state :step 2)
-;;     (goto-char (point-min)))
-;;    ;; 3回目: 元の位置へ戻る
-;;    (t
-;;     (goto-char (plist-get my-smart-jump-state :origin))
-;;     (setq this-command nil)))) ; 状態リセット
+(defun my-smart-beginning-of-line ()
+  "Toggle between beginning of line, beginning of buffer, and original point."
+  (interactive)
+  (cond
+   ;; 1回目: 行頭へ移動し、元の位置を保存
+   ((not (eq last-command 'my-smart-beginning-of-line))
+    (setq my-smart-jump-state (list :origin (point) :step 1))
+    (move-beginning-of-line 1))
+   ;; 2回目: バッファ先頭へ
+   ((= (plist-get my-smart-jump-state :step) 1)
+    (plist-put my-smart-jump-state :step 2)
+    (goto-char (point-min)))
+   ;; 3回目: 元の位置へ戻る
+   (t
+    (goto-char (plist-get my-smart-jump-state :origin))
+    (setq this-command nil)))) ; 状態リセット
 
-;; (defun my-smart-end-of-line ()
-;;   "Toggle between end of line, end of buffer, and original point."
-;;   (interactive)
-;;   (cond
-;;    ((not (eq last-command 'my-smart-end-of-line))
-;;     (setq my-smart-jump-state (list :origin (point) :step 1))
-;;     (move-end-of-line 1))
-;;    ((= (plist-get my-smart-jump-state :step) 1)
-;;     (plist-put my-smart-jump-state :step 2)
-;;     (goto-char (point-max)))
-;;    (t
-;;     (goto-char (plist-get my-smart-jump-state :origin))
-;;     (setq this-command nil))))
+(defun my-smart-end-of-line ()
+  "Toggle between end of line, end of buffer, and original point."
+  (interactive)
+  (cond
+   ((not (eq last-command 'my-smart-end-of-line))
+    (setq my-smart-jump-state (list :origin (point) :step 1))
+    (move-end-of-line 1))
+   ((= (plist-get my-smart-jump-state :step) 1)
+    (plist-put my-smart-jump-state :step 2)
+    (goto-char (point-max)))
+   (t
+    (goto-char (plist-get my-smart-jump-state :origin))
+    (setq this-command nil))))
 
-;; (global-set-key (kbd "C-a") #'my-smart-beginning-of-line)
-;; (global-set-key (kbd "C-e") #'my-smart-end-of-line)
+(defun other-window-or-split ()
+  (interactive)
+  (when (one-window-p)
+    (split-window-horizontally))
+  (other-window 1))
